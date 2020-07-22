178ebe1b6dc44947ddaf185aa76371c233404902
Evgeny Cherner <Eugene.Cherner@dataart.com>
2020-07-22 20:13:02 +0300

    added BaseDTO

--- src/main/java/com/deckofcardsapi/dto/BaseDTO.java
+++ src/main/java/com/deckofcardsapi/dto/BaseDTO.java
@@ -4,15 +4,18 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class BrandNewDeckDTO extends BaseDTO {
+public class BaseDTO {
 
-    @JsonProperty("shuffled")
-    private Boolean shuffled;
+    @JsonProperty("success")
+    private Boolean success;
 
+    @JsonProperty("deck_id")
+    private String deckId;
+
+    @JsonProperty("remaining")
+    private Integer remaining;
 }
